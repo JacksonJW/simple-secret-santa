d4cac7097ad90e0121274e3f641082ea83aff487
JacksonJW <jacksonjwatkins@gmail.com>
2022-12-04 16:28:06 -0800

    reword unpaired participants prompt #2

--- simple_secret_santa.py
+++ simple_secret_santa.py
@@ -43,7 +43,7 @@ if __name__ == "__main__":
 
     participants_user_input = input("Enter in participants separated by spaces: \n")
     unpaired_participants_user_input = input(
-        "Enter groups that cannot be paired w/ eachother separated by commas: \n"
+        "Enter groups for which members cannot be paired w/ eachother separated by commas: \n"
     )
 
     participants_input = participants_user_input.split(" ")
